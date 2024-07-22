898403b0ee8a13a6bd7dc3a05ec1c3d5f5785148
SwitteeFranca2-0 <switteefrankie@gmail.com>
2024-07-22 16:30:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 class Config:
     """Configuration details"""
-    ACCOUNT_EMAIL = 'clientfreelancer3@gmail.com'
-    ACCOUNT_PASSWORD = 'Notmyaccount'
+    ACCOUNT_EMAIL = 'joshbrooks2022@gmail.com'
+    ACCOUNT_PASSWORD = 'Testing2024!'
     ACCOUNT_NAME = 'Joshi Burner'
     chromedriver_path = 'driver/chromedriver'
+
+
+
+
+
+
+
